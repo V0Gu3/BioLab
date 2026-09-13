e39f0b60b10a03a577c687c86955e1ffedab6350
V0Gu3 <vogueson@gmail.com>
2026-09-13 02:55:27 -0600

    Expone rutas de autenticación explícitas en Vercel

--- api/[...path].js
+++ api/[...path].js
@@ -1,8 +1,1 @@
-'use strict';
-const { createApp } = require('../server/server');
-
-let application;
-module.exports = async (request, response) => {
-  application ||= await createApp();
-  application.emit('request', request, response);
-};
+module.exports = require('./_handler');
---
+++ api/_handler.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { createApp } = require('../server/server');
+
+let application;
+module.exports = async (request, response) => {
+  application ||= await createApp();
+  application.emit('request', request, response);
+};
